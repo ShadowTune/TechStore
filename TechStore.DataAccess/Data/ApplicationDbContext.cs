083b2d784cf572d0898505278e7d7d46b8bfb80c
CSE-3206 <rifatphyisicist1@gmail.com>
2024-12-03 15:36:16 +0600

    Fixed

---
+++ TechStore.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechStore.Models;
+
+namespace TechStore.DataAccess.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+			
+		}
+		public DbSet<Category> Categories { get; set; }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			// base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Category>().HasData(
+				new Category { Id = 1, Name = "LENOVO", DisplayOrder = 12},
+				new Category { Id = 2, Name = "ASUS", DisplayOrder = 5 },
+				new Category { Id = 3, Name = "ACER", DisplayOrder = 3 },
+				new Category { Id = 4, Name = "DELL", DisplayOrder = 4 },
+				new Category { Id = 5, Name = "MSI", DisplayOrder = 6 },
+				new Category { Id = 6, Name = "HP", DisplayOrder = 2 }
+			);
+		}
+	}
+}
---
+++ TechStore.DataAccess/Migrations/20241202143911_SeedCategoryTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TechStore.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 12, "LENOVO" },
+                    { 2, 5, "ASUS" },
+                    { 3, 3, "ACER" },
+                    { 4, 4, "DELL" },
+                    { 5, 6, "MSI" },
+                    { 6, 2, "HP" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
