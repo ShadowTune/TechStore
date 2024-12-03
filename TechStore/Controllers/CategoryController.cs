1475ba2579818eff808491dfa6771df8f66518ed
Rifat-IBN-Azad <rifatphysicist1@gmail.com>
2024-12-03 08:37:33 +0600

    nbjub

--- TechStore/Controllers/CategoryController.cs
+++ TechStore/Controllers/CategoryController.cs
@@ -16,14 +16,27 @@ namespace TechStore.Controllers
 			List<Category> objCategoryList = _db.Categories.ToList();
 			return View(objCategoryList);
 		}
-		public IActionResult Add()
+		public IActionResult Add(int? id)
 		{
-			return View();
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category category = _db.Categories.FirstOrDefault(c => c.Id == id);
+			return View(category);
 			// not required to pass an object
 		}
 		[HttpPost]
 		public IActionResult Add(Category category)
 		{
+			if (category.Name == category.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("Name", "Name and Orders must be different");
+			}
+			if (category.Name.ToLower() == "test")
+			{
+				ModelState.AddModelError("Name", "Name is invalid");
+			}
 			if (ModelState.IsValid)
 			{
 				_db.Categories.Add(category);
@@ -31,6 +44,66 @@ namespace TechStore.Controllers
 				return RedirectToAction("Index");
 			}
 			return View(category);
+			// not required to pass an object
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category category = _db.Categories.FirstOrDefault(c => c.Id == id);
+			return View(category);
+			// not required to pass an object
+		}
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+			if (category.Name == category.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("Name", "Name and Orders must be different");
+			}
+			if (category.Name.ToLower() == "test")
+			{
+				ModelState.AddModelError("Name", "Name is invalid");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(category);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(category);
+			// not required to pass an object
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category category = _db.Categories.Find(id);
+			return View(category);
+			// not required to pass an object
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+			Category category = _db.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			// _db.Categories.Remove(category);
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Remove(category);
+				_db.SaveChanges();
+			}
+			return RedirectToAction("Index");
+			// return View(category);
 			// not required to pass an object
 		}
 	}
